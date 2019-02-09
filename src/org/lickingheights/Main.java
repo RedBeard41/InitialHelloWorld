fd2169c9aac80d450686642a21329332bf0fa1fe
Gabriel Walters <gjwalters@fullsail.edu>
2019-02-08 23:50:47 -0500

    created intellij project

---
+++ src/org/lickingheights/Main.java
@@ -0,0 +1,12 @@
+package org.lickingheights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
